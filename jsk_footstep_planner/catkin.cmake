9319e715b75c32bd1d3bb4705e606f06afe6ff60
Kei Okada <kei.okada@gmail.com>
2014-10-11 14:52:18 +0900

    Revert "Trimmed the branch to only the eus_qpoases sub directory"
    This reverts commit eab70741b57ee65e94aa441305f97551a1e8aece.

---
+++ jsk_footstep_planner/catkin.cmake
@@ -0,0 +1,17 @@
+cmake_minimum_required(VERSION 2.8.3)
+project(jsk_footstep_planner)
+
+find_package(catkin REQUIRED COMPONENTS jsk_footstep_msgs jsk_pcl_ros roseus)
+#catkin_python_setup()
+catkin_package(
+  DEPENDS
+  CATKIN-DEPENDS  jsk_footstep_msgs jsk_pcl_ros roseus
+  INCLUDE_DIRS # TODO include
+  LIBRARIES # TODO
+  )
+
+install(DIRECTORY euslisp launch scripts
+  DESTINATION ${CATKIN_PACKAGE_SHARE_DESTINATION}
+  USE_SOURCE_PERMISSIONS
+  PATTERN ".svn" EXCLUDE
+  )
