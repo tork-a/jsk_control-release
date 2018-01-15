8e07ec593946babc15ffe5c6c2b8f595305a036d
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2018-01-15 04:54:02 +0000

    Trimmed the branch to only the eus_qp sub directory

--- jsk_calibration/hrp2jsknt_calibration/view_results/view_scatter.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rosrun calibration_estimation error_visualization.py /tmp/hrp2jsknt_calibration/cal_measurements.bag /tmp/hrp2jsknt_calibration/ `rospack find jsk_calibration`/hrp2jsknt_calibration/view_results/scatter_config.yaml
